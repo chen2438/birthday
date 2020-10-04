2f37b056a1ecafe9f345edb1207b84fc39b93343
chen2438 <2363822647@qq.com>
2020-10-04 17:07:44 +0800

    0

--- config.js
+++ config.js
@@ -9,9 +9,6 @@ var config = {
         "Joyeuse anniversaire",
         "誕生日おめでとうございます",
         "С днем рождения",
-        "Wszystkiego najlepszego",
-        "Herzlichen Glückwunsch",
-        "Buon compleanno",
         "GL & HF !",
     ],
     /*
@@ -20,7 +17,7 @@ var config = {
      */
     imgs: {
         //"心爱的小可爱": "./imgs/xiaokeai.png",
-        // "今天是你的生日": "./imgs/birthday.jpg",
+        "今天是你的生日": "./imgs/bd1.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
